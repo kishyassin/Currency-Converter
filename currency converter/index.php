3620a0c80d993baa4b2ef88559221b3b212654ca
KICH Yassine <kishyassin@gmail.com>
2024-03-02 13:43:28 +0100

    Rename File Edite title

--- currency converter/index.php
+++ currency converter/index.php
@@ -23,7 +23,7 @@ include 'inputModulPro.php'
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Currency Exchange</title>
     <style>
         * {
             padding: 0;
